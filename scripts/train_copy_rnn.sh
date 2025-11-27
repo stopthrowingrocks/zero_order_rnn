126fb565c1a5c9670c07272ce66a5c162f12514c
Marcus Luebke <>
2025-11-26 20:12:08 -0800

    ignore logs

--- scripts/train_copy_rnn.sh
+++ scripts/train_copy_rnn.sh
@@ -6,4 +6,4 @@ TINY_HEAD_SIZE=20
 TINY_BATCH=2048
 TINY_LR=1
 
-python -m torch.distributed.launch --nproc_per_node=2 --master_port=29501 distributed_rge.py --model_type LSTM --hidden_size $TINY_HIDDEN --num_heads $TINY_HEADS --head_size $TINY_HEAD_SIZE --batch_size $TINY_BATCH --learning_rate $TINY_LR --mode=train --wandb_proj zero_order_rnn
+python -m torch.distributed.launch --nproc_per_node=8 --master_port=29501 distributed_rge.py --model_type LSTM --hidden_size $TINY_HIDDEN --num_heads $TINY_HEADS --head_size $TINY_HEAD_SIZE --batch_size $TINY_BATCH --learning_rate $TINY_LR --mode=train --wandb_proj zero_order_rnn
